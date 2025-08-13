7c4e1c64c79566cad71dc346005de0c9d2aa84c1
Rishabh Patel <rishabhpatelme01@gmail.com>
2025-08-13 18:35:57 +0530

    Add files via upload

---
+++ Oddoreven.java
@@ -0,0 +1,8 @@
+public class Oddoreven {
+  public static void main(String args[]) {
+  
+    int num = 14;
+    String oddOrEven = (num % 2 == 0) ? "even" : "odd";    
+    System.out.println(oddOrEven);
+  }
+}
