a259a7aba7c4697778d543f2f1bff7569cf8e5aa
Rishabh Patel <rishabhpatelme01@gmail.com>
2025-08-15 00:01:54 +0800

    Add files via upload

---
+++ Typeconversionandcasting.java
@@ -0,0 +1,24 @@
+public class Typeconversionandcasting {
+  public static void main(String args[]) {
+    
+    byte b = 127;
+    int i = b;
+
+     System.out.println(i);
+    
+    int a = 257;
+    byte t = (byte) a;
+
+    System.out.println(t);
+     
+    int c = 192;
+    float f = c;
+
+    System.out.println(f);
+
+    float l = 21.90f;
+    int h = (int) l;
+
+    System.out.println(h);
+  }
+}
