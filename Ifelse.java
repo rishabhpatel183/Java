cf69f09985e4e01139d60cca539f4a8d7ce52e11
Rishabh Patel <rishabhpatelme01@gmail.com>
2025-08-15 21:48:50 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,16 @@
+public class Ifelse {
+
+    public static void main(String[]  args){
+        
+        int a = 4;
+        int b = 6;
+        int c = 9;
+
+        if (a > b){
+        System.out.println("a is greater then b ");}
+        else {
+            System.out.println("a is smaller then b ");   
+        }
+
+    }
+}
