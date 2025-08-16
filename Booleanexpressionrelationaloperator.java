9ab2e777b83675ecd312eea5d650e247f73b8a33
Rishabh Patel <rishabhpatelme01@gmail.com>
2025-08-16 20:42:24 +0530

    Add files via upload

---
+++ Booleanexpressionrelationaloperator.java
@@ -0,0 +1,27 @@
+public class Booleanexpressionrelationaloperator {
+    public static void main(String args[]) {
+        int a = 4;
+        int b = 4;
+        int c = 6;
+        float d = 4.4f;
+        float e = 6.6f;
+        
+        boolean res = a == b;
+        boolean result = b == c;
+        boolean ans = b != c;
+        boolean comparison = a <= d;
+
+        System.out.println(res);
+        System.out.println(result);
+        System.out.println(ans);
+        System.out.println(comparison);
+
+        
+         int x = 10;
+         int y = 9;
+         System.out.println(x > y); // returns true, because 10 is higher than 9  
+            
+          
+          
+    }
+}
